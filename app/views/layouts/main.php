b2e3ca2b9de3c35d66669beaf3ce39a750cad0fc
Ladlen <TwilightTower@mail.ru>
2019-09-13 13:04:09 +0300

    htmlEncode() for title

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?= $this->title ?></title>
+    <title><?= \app\core\html\Safe::htmlEncode($this->title) ?></title>
 
     <link href="css/main.css" rel="stylesheet" media="all" />
 
