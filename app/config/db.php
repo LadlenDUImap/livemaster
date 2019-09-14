9e1d58c7582416044b47c32373e48a55d925f56d
Ladlen <TwilightTower@mail.ru>
2019-09-15 00:23:55 +0300

    db encoding fix

--- app/config/db.php
+++ app/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'app\core\db\Pdo',
-    'dsn' => 'mysql:host=localhost;dbname=livemaster',
+    'dsn' => 'mysql:host=localhost;dbname=livemaster;charset=utf8mb4',
     'username' => 'root',
     'password' => 'temp12345',
-    'charset' => 'utf8md4',
+    //'charset' => 'utf8md4',
 ];
