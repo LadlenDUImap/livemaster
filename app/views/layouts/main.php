adf75d32c5dd7bba722b9a8aa5e4daa9d2f8b652
Ladlen <TwilightTower@mail.ru>
2019-10-16 08:04:14 +0300

    css fixes

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -15,7 +15,7 @@ use app\core\Url;
 
     <title><?= \app\core\html\Safe::htmlEncode($this->title) ?></title>
 
-    <link href="<?= Url::to('/css/bootstrap-grid.css') ?>" rel="stylesheet" media="all" />
+    <!--<link href="<?/*= Url::to('/css/bootstrap-grid.css') */?>" rel="stylesheet" media="all" />-->
     <link href="<?= Url::to('/css/main.css') ?>" rel="stylesheet" media="all" />
 
     <?= $this->css ?>
