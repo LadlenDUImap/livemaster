a438e3a848126579bf130c1f68385c32e42bbea0
Ladlen <TwilightTower@mail.ru>
2019-11-15 05:49:20 +0300

    manual PSR-0 for spl_autoload_register()

--- web/index.php
+++ web/index.php
@@ -21,7 +21,22 @@ if (LM_DEBUG) {
 
 $config = require APP_DIR . 'config/app.php';
 
+spl_autoload_extensions('.inc,.php');
+
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../'));
-spl_autoload_register();
+
+// PSR-0
+spl_autoload_register(function ($className) {
+    $className = ltrim($className, '\\');
+    $fileName = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+});
 
 \app\core\Lm::inst()->run($config);
