1f10a05346d46c8278a3502edde4c071f7c842b2
Ladlen <TwilightTower@mail.ru>
2019-12-12 13:31:50 +0300

    db_test.sql добавлен. README.md

---
+++ app/data/db/db_test.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS `livemaster_test`
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+USE `livemaster_test`;
+
+DROP TABLE IF EXISTS `cities`;
+CREATE TABLE `cities` (
+  `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30)      NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id`      INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`    VARCHAR(30)      NOT NULL,
+  `age`     INT UNSIGNED     NOT NULL,
+  `city_id` INT(11) UNSIGNED NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`)
+)
+  ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
